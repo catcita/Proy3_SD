97be80f4e26de0968d7640746aa61b19275bf9b9
Camilo Fuentes <camilo.fuentes.01.03.20@gmail.com>
2025-11-30 14:36:15 -0300

    Me galtaron weas en el commit anterior

--- app2/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-very-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://user:password@mariadb/invoicing'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    RABBITMQ_URL = os.environ.get('RABBITMQ_URL') or 'amqp://guest:guest@middleware:5672/'
